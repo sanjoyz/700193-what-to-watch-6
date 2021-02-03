f01edd6f6d4d884b5fe47863c5dbbb9cb6c6f7f4
sanjoyz <sanjoyz1105@gmail.com>
2021-02-03 22:19:33 +0300

    прокидывает пропсы для промо фильма в mainscreen

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import MainScreen from '../main/main-screen';
+import PropTypes from 'prop-types';
 
 const App = (props) => {
   const {movieCardsCount} = props;
   return (
     <MainScreen movieCardsCount={movieCardsCount}
+      promoFilmName={props.promoFilmName}
+      promoFilmGenre={props.promoFilmGenre}
+      promoFilmReliseYear={props.promoFilmReliseYear}
     />
   );
+};
+App.propTypes = {
+  movieCardsCount: PropTypes.number.isRequired,
+  promoFilmName: PropTypes.string.isRequired,
+  promoFilmGenre: PropTypes.string.isRequired,
+  promoFilmReliseYear: PropTypes.number.isRequired
 };
 
 export default App;
