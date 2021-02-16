7b190f45b00643476c197cd899a8d06cd9aa70a7
sanjoyz <sanjoyz1105@gmail.com>
2021-02-16 21:49:34 +0300

    фиксит опечатку

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const MovieCard = (props) => {
-  const name = props.name
+  const name = props.name;
   const previewImage = props.previewImage;
   return (
     <React.Fragment>
       <article className="small-movie-card catalog__movies-card">
         <div className="small-movie-card__image">
-          <img src={previewImage} alt="Fantastic Beasts: The Crimes of Grindelwald" width={280} height={175} />
+          <img src={previewImage} alt="Fantastic Beasts: The Crimes of Grindelwald" width={280} height={175}/>
         </div>
         <h3 className="small-movie-card__title">
           <a className="small-movie-card__link" href="movie-page.html">{name}</a>
@@ -17,7 +17,7 @@ const MovieCard = (props) => {
     </React.Fragment>
   );
 };
-MovieCard.PropTypes = {
+MovieCard.propTypes = {
   previewImage: PropTypes.string,
   name: PropTypes.string
 };
