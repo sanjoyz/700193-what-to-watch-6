f284f8615d62a0b25e0cd9059ba9fda429eb539f
sanjoyz <sanjoyz1105@gmail.com>
2021-02-21 14:45:48 +0300

    создаст action и reducer

---
+++ src/store/action.js
@@ -0,0 +1,4 @@
+export const AtionType = {
+  CHANGE_GENRE: `filter/changeGenre`,
+  GET_FILMS_LIST: `filter/getFilmList`
+};
---
+++ src/store/reducer.js
@@ -0,0 +1,27 @@
+import {ActionType} from "./action";
+const initState = {
+  genre: ``,
+  filmList: []
+};
+
+const reducer = (state = initState, action) => {
+  switch (action.type) {
+    case ActionType.CHANGE_GENRE:
+      return {
+        ...state,
+        genre: action.payload
+      };
+    case ActionType.GET_FILMS_LIST:
+      return {
+        ...state,
+        filmList: action.payload
+
+      };
+
+    default:
+      break;
+  }
+  return state;
+};
+
+export default {reducer};
