08e950d48352a8b8f204f5fb4ca1f9449b5ca5d4
sanjoyz <sanjoyz1105@gmail.com>
2021-02-24 23:34:43 +0300

    добавит логику в фильтр по жанрам

--- src/components/genre-list/genre-list.jsx
+++ src/components/genre-list/genre-list.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {ActionCreator} from '../../store/action';
+import {connect} from 'react-redux';
+
 const GenreList = (props) => {
-  const {films} = props;
+  const {films, onGenreChange} = props;
   const genres = [];
   films.forEach((film) => {
     genres.push(film.genre);
@@ -11,12 +14,12 @@ const GenreList = (props) => {
     <React.Fragment>
       <ul className="catalog__genres-list">
         <li className="catalog__genres-item catalog__genres-item--active">
-          <a href="#" className="catalog__genres-link">All genres</a>
+          <a href="#" onClick={onGenreChange} className="catalog__genres-link">All genres</a>
         </li>
         {genres.map((genre) => {
           return (
-            <li key={genre} className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">{genre}</a>
+            <li key={genre + Math.random()} className="catalog__genres-item">
+              <a href="#" onClick={onGenreChange} className="catalog__genres-link">{genre}</a>
             </li>
           );
         })}
@@ -27,7 +30,16 @@ const GenreList = (props) => {
 
 GenreList.propTypes = {
   films: PropTypes.array,
+  onGenreChange: PropTypes.func
 };
 
-export default GenreList;
+const mapDispatchToProps = (dispatch) => ({
+  onGenreChange(genre) {
+    dispatch(ActionCreator.getFilmList(genre));
+  }
+});
+
+export {GenreList};
+export default connect(null, mapDispatchToProps)(GenreList);
+
 
