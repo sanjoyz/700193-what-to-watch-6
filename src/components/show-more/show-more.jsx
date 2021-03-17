9c75c962e0eee3749c4291f11fca96b0299a5b1e
sanjoyz <sanjoyz1105@gmail.com>
2021-03-17 17:08:48 +0300

    создаст компонент show-more

---
+++ src/components/show-more/show-more.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ShowMore = (props) => {
+
+  const {filmsShown, films, showMoreHanlder} = props;
+
+  return (
+    filmsShown >= films.length ? ` ` :
+      <React.Fragment>
+        <div className="catalog__more">
+          <button className="catalog__button" onClick={showMoreHanlder} type="button">Show more</button>
+        </div>
+      </React.Fragment>
+  );
+};
+
+ShowMore.propTypes = {
+  filmsShown: PropTypes.number.isRequired,
+  films: PropTypes.array.isRequired,
+  showMoreHanlder: PropTypes.func,
+};
+
+export default ShowMore;
+
