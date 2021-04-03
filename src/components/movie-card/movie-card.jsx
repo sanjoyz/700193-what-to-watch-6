226aaa25abed962449373293200dadf14ee7beab
sanjoyz <sanjoyz1105@gmail.com>
2021-04-03 11:04:17 +0300

    баг когда стейт не успевал обновляться из-за ререндера карточки

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 import VideoPlayer from '../videoplayer/video-player';
@@ -8,26 +8,32 @@ import {AppRoute} from '../../const';
 const MovieCard = (props) => {
 
   const {name, previewImage, posterImage, videoLink, id} = props;
-  const [focusedCard, setFocused] = useState(null);
 
-  const onHoverHanlder = React.useCallback((i) => {
-    setFocused(i);
-  }, [focusedCard]);
+  const [focusedCard, setfocusedCard] = useState(null);
+  const [nextFilmId, setNextFilmId] = useState(null);
 
-  let timer;
+
+  const handleActiveFilmChange = useCallback((i) => {
+    setNextFilmId(i);
+    if (i === null) {
+      setfocusedCard(i);
+    }
+  }, []);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setfocusedCard(nextFilmId);
+    }, 1000);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [nextFilmId]);
+
   return (
     <React.Fragment>
       <article className="small-movie-card catalog__movies-card"
-        onMouseOver={() => {
-          timer = setTimeout(() => {
-            onHoverHanlder(id);
-          }, 1000);
-
-        }}
-        onMouseLeave={() => {
-          onHoverHanlder(null);
-          clearTimeout(timer);
-        }}
+        onMouseEnter={() => handleActiveFilmChange(id)}
+        onMouseLeave={() => handleActiveFilmChange(null)}
       > <Link className="small-movie-card__link" to={{pathname: `${AppRoute.FILMS}/${id}`}}>
           <div className="small-movie-card__image">
             {id === focusedCard
